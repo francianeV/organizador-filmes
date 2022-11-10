f60d4d3cba4a7dcd94d9f02d65958f1e04f26665
Franciane <franciane.penina@gmail.com>
2022-11-10 02:35:25 -0300

    connection with the database established

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const server = express();
 
 server.listen(process.env.PORT, ()=> {console.log(`Listening on PORT ${process.env.PORT}`)});
+
