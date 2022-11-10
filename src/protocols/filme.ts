b6f6759f3bda5b0861c29cbb3c32f056672fbfdf
Franciane <franciane.penina@gmail.com>
2022-11-10 02:41:38 -0300

    create types to movie and its entity

---
+++ src/protocols/filme.ts
@@ -0,0 +1,10 @@
+export type FilmeEntity = {
+    id: number,
+    nome: string,
+    genero: string,
+    plataformaId: number,
+    status: boolean,
+    nota: string
+}
+
+export type Filme = Omit<FilmeEntity, "id">
